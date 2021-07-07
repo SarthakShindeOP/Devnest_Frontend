a2e4d4aa982f19b8ee1b1cc5420db1c3ec506bd6
Sarthak <68957436+SarthakShindeOP@users.noreply.github.com>
2021-07-07 12:03:50 +0530

    Update day7.js

--- day7/day7.js
+++ day7/day7.js
@@ -1,40 +1,41 @@
 // // first question 
-// var student = { name : "David Rayy", sclass : "VI", rollno : 12 };
-// // console.log((Object.keys(student)).join())
+var student = { name : "David Rayy", sclass : "VI", rollno : 12 };
+console.log((Object.keys(student)).join())
 
 
 // // second question
-// var student = { name : "David Rayy", sclass : "VI", rollno : 12 }; 
-// delete student.rollno
-// console.log(student);
+var student = { name : "David Rayy", sclass : "VI", rollno : 12 }; 
+delete student.rollno
+console.log(student);
 
 
 //third question 
-// var student = { name : "David Rayy", sclass : "VI", rollno : 12 };
-// console.log( Object.keys(student).length);
+var student = { name : "David Rayy", sclass : "VI", rollno : 12 };
+console.log( Object.keys(student).length);
 
 
 
 //fourth question
-// var library = [ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true }, { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }]; 
+var library = [ { author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true }, { author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true }, { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', readingStatus: false }]; 
 
-// console.log(library.map(item=>{
-//     console.log( (Object.values(item).join()))
-// }));
+console.log(library.map(item=>{
+    console.log( (Object.values(item).join()))
+}));
 
 
 // fifth question 
-// const cylinder=
-// {
-//     r:0,
-//     h:0,
-//     volume(){
-//     return ((22/7)*this.r*this.h).toFixed(4);
-// },};
-// cylinder.r=2;
-// cylinder.h=2;
-// console.log(cylinder.volume());
+const cylinder=
+{
+    r:0,
+    h:0,
+    volume(){
+    return ((22/7)*this.r*this.h).toFixed(4);
+},};
+cylinder.r=2;
+cylinder.h=2;
+console.log(cylinder.volume());
 
+// sixth question 
 var library = [ { title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 }, 
                 { title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, 
                 { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245 }]; 
